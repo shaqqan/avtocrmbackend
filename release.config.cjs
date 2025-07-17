ad23e29ee4878d40502474c5de66735d0a9b5f47
a.berdimuratov <a.berdimuratov@uzinfocom.uz>
2025-07-17 11:48:27 +0500

    chore: update GitLab CI configuration and add semantic release support
    - Changed the Node.js image in the `release` job from `node:20-alpine` to `node:20-bullseye` for better compatibility.
    - Updated the `notify_on_merge_request` job image from `curlimages/curl:latest` to `busybox:latest`.
    - Added a `release` script to `package.json` for semantic release automation.
    - Included new dependencies for semantic release in `package.json` and `pnpm-lock.yaml`.
    - Modified the `release.config.cjs` to configure semantic release plugins and commands for changelog generation and GitLab integration.

--- release.config.cjs
+++ release.config.cjs
@@ -3,33 +3,42 @@ const config = require('semantic-release-preconfigured-conventional-commits');
 const publishCommands = `
 git tag -a -f \${nextRelease.version} \${nextRelease.version} -F CHANGELOG.md || exit 2
 git push --force origin \${nextRelease.version} || exit 3
-echo "release_status=released" >> $GITHUB_ENV
-echo "CONTAINER_VERSION="\${nextRelease.version} >> $GITHUB_ENV
 `;
 
 const releaseBranches = ['main'];
 config.branches = releaseBranches;
 
-config.plugins.push(
+config.plugins = [
+    '@semantic-release/commit-analyzer',
+    '@semantic-release/release-notes-generator',
+    [
+        '@semantic-release/changelog',
+        {
+            changelogFile: 'CHANGELOG.md',
+            changelogTitle: '# Changelog\n\nAll notable changes to this project will be documented in this file.'
+        }
+    ],
     [
         '@semantic-release/exec',
         {
+            prepareCmd: 'pnpm install && pnpm build',
             publishCmd: publishCommands,
-        },
-    ],
-    [
-        '@semantic-release/github',
-        {
-            assets: [],
         },
     ],
     [
         '@semantic-release/git',
         {
             assets: ['CHANGELOG.md', 'package.json'],
-            message: 'chore(release)!: [skip ci] ${nextRelease.version} released',
-        },
+            message: 'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}'
+        }
     ],
-);
+    [
+        "@semantic-release/gitlab",
+        {
+            "gitlabUrl": "https://gitlab.uzinfocom.uz",
+            "gitlabApiPathPrefix": "/api/v4"
+        }
+    ]
+];
 
 module.exports = config;
