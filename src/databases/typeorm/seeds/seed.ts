bb28ef2cb60873cfa0d8256e87757d35872445a9
a.berdimuratov <a.berdimuratov@uzinfocom.uz>
2025-08-01 18:09:08 +0500

    refactor: clean up seed file by removing unused seeder imports
    - Removed commented-out imports for PermissionSeeder, UserSeeder, and RoleSeeder from the seed.ts file.
    - Retained the LanguageSeeder import and its seeding functionality for continued use.

--- src/databases/typeorm/seeds/seed.ts
+++ src/databases/typeorm/seeds/seed.ts
@@ -1,24 +1,11 @@
 import { config } from 'dotenv';
-
 import dataSource from '../data-source';
-import { PermissionSeeder } from './permission.seeder';
-import { UserSeeder } from './user.seeder';
-import { RoleSeeder } from './role.seeder';
 import { LanguageSeeder } from './language.seeder';
 
 config();
 async function seed() {
   try {
     await dataSource.initialize();
-
-    // const permissionSeeder = new PermissionSeeder(dataSource);
-    // await permissionSeeder.seed();
-
-    // const roleSeeder = new RoleSeeder(dataSource);
-    // await roleSeeder.seed();
-
-    // const userSeeder = new UserSeeder(dataSource);
-    // await userSeeder.seed();
 
     const languageSeeder = new LanguageSeeder(dataSource);
     await languageSeeder.seed();
